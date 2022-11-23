8d03de32f74bdcfca330954e9b1c3e56b3593e17
yanochkina_sl <yanochkinasvetlana@mail.ru>
2022-11-23 17:33:53 +0600

    Add sourse and tests

---
+++ stars/stars/get_value.py
@@ -0,0 +1,8 @@
+
+def get_val(collectino, key, default = None):
+    if key in list(collection.keys()):
+        print(collection[key])
+    else:
+        print(default)
+
+
---
+++ stars/tests/test_get_value.py
@@ -0,0 +1,14 @@
+from stars.get_value import get_val
+
+def test_get_val():
+    assert get_val({1:'Show', 2:'must', 3:'go on'}, 3, 'Queen') == 'go on'
+
+def test_get_val():
+    assert get_val({1:'Show', 2:'must', 3:'go on'}, 5, 'Queen') == 'Queen'
+
+
+def test_ampty_coll():
+    assert get_val({}, 2, 'Wtf') == 'Wtf'
+    assert get_val({1:'Show', 2:'must', 3:'go on'}, 5) == None
+
+
